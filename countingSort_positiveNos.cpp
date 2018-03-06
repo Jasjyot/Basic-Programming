a3b77aaa85579dbddf81bf254234b6e885901c21
Jasjyot Singh <jasjyot98@gmail.com>
2018-03-06 17:09:29 +0530

    Create countingSort_positiveNos.cpp

---
+++ countingSort_positiveNos.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void countingSort(int arr[], int n)
+{
+    int max=*max_element(arr,arr+n);
+    int freq[max+1]={0};
+    for(int i=0;i<n;i++)
+    {
+        freq[arr[i]]++;
+    }
+    
+    for(int i=0,j=0 ; (i<max+1 && j<n) ;i++)
+    {
+        while(freq[i]!=0)
+        {
+            arr[j]=i;
+            j++;
+            freq[i]--;
+        }
+    }
+}
+
+
+int main() 
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];        
+    }
+    
+    countingSort(arr,n);
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<endl;        
+    }
+    
+    return 0;
+}
