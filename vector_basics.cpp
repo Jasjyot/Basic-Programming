94de702ccb608ba2e5c5aebbd58fa495e6d6ce5c
Jasjyot Singh <jasjyot98@gmail.com>
2018-05-08 20:45:17 +0530

    Update vector_basics.cpp

--- vector_basics.cpp
+++ vector_basics.cpp
@@ -12,6 +12,25 @@ bool compare(int a, int b)
 
 int main() 
 {
+    vector<int> v1={1,2,3,4};
+    vector<int> v2={5,6,7,8,9};
+    
+    v1.swap(v2);
+    
+    for(int i=0;i<v1.size();i++)
+    {
+        cout<<v1[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<v2.size();i++)
+    {
+        cout<<v2[i]<<" ";
+    }
+    
+    
+    cout<<"\n\n\n\n";
+    
+    
     vector<int> v;
     v.reserve(100);
     int n,temp;
